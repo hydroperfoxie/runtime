202399c9f0ea592b3c995e5ec57ff9b2875c5bb4
hydroperfox <hydroperfox@gmail.com>
2024-08-07 15:26:43 -0300

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,11 @@ export default {
     input: "src/index.ts",
     output: [
         {
-            file: "target/iron.js",
+            file: "target/skyflex.js",
             format: "es",
         },
         {
-            file: "target/iron.min.js",
+            file: "target/skyflex.min.js",
             format: "es",
             name: "version",
             plugins: [terser()],
